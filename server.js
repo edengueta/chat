578ea12a90f6b1c3c058ec4ac4677065c8ad790c
עדן גואטה <eden@h-MacBook-Pro-sl-dn.local>
2020-10-02 17:32:22 +0300

    better uuid

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
+const { v4: uuidv4 } = require('uuid');
 
 let history = require('./history.json') || [];
-let uuid = history.length > 0 ? history[history.length - 1].id + 1 : 0;
 
 function saveHistory(history) {
     fs.writeFileSync('history.json', JSON.stringify(history), { encoding: 'utf-8' });
@@ -35,6 +35,7 @@ app.delete('/messages/:id', (req, res) => {
     let id = req.params.id;
     let messageToRemove = history.findIndex(message => message.id == id);
     history.splice(messageToRemove, 1);
+    console.log("removing- message-index: ", messageToRemove, "message-id: ", id);
     saveHistory(history);
     res.send(history);
 
@@ -45,16 +46,16 @@ app.put('/messages/:id/:new', (req, res) => {
     let messageToEdit = history.findIndex(message => message.id == id);
     history[messageToEdit].message = req.params.new;
     history[messageToEdit].edited = "(edited)";
+    console.log("editing- message-index: ", messageToEdit, "message-id: ", id);
     saveHistory(history);
     res.send(history);
 });
 
 app.post('/messages', (req, res) => {
     history.push({
-        id: uuid,
+        id: uuidv4(),
         ...req.body,
     });
-    uuid++;
     saveHistory(history);
     res.send(history);
 });
